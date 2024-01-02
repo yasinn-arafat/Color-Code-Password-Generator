f84f217f9f35aecd5d1f2c3ab67277f6b21fc30c
yasinarafat4870 <yasinarafat4870@gmail.com>
2024-01-02 13:07:46 +0600

    Pasword Js part done

--- main.js
+++ main.js
@@ -35,3 +35,42 @@ function copyColor() {
   Cbtn.innerHTML = "Copied &check;";
   Cbtn.style.color = "#ff4655";
 }
+
+//  Random Password Generator  ------->
+
+const displayPass = document.querySelector("#displayPass");
+const gpBtn = document.querySelector(".gpBtn");
+const cpBtn = document.querySelector(".cpBtn");
+
+const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowercase = uppercase.toLocaleLowerCase();
+const charecter = `"~!@#$%^&*()-=+_;:<,>.?/|{}[]`;
+const number = "1234506789";
+
+// Make a long string combination of uppercase, lowercase, number, charecter
+
+const allcharecter = uppercase + lowercase + charecter + number;
+const passwordLength = 10;
+
+// Add Event listener in Generate Buttton
+
+gpBtn.addEventListener("click", function () {
+  const upper = uppercase[Math.floor(Math.random() * uppercase.length)];
+  const lower = lowercase[Math.floor(Math.random() * lowercase.length)];
+  const char = charecter[Math.floor(Math.random() * charecter.length)];
+  const num = number[Math.floor(Math.random() * number.length)];
+  let password = lower + num + upper + char;
+  while (password.length <= passwordLength) {
+    password += allcharecter[Math.floor(Math.random() * allcharecter.length)];
+  }
+  displayPass.value = password;
+});
+
+//  Copy Password Function
+
+cpBtn.addEventListener("click", copyPass);
+function copyPass() {
+  navigator.clipboard.writeText(displayPass.value);
+  cpBtn.innerHTML = "Copied &check;";
+  cpBtn.style.color = "#ff4655";
+}
